f19012fae84c8705c5f1c32aeb2da9b2b4324699
Giacomo Caria <giacomo@chloris.earth>
2026-01-22 11:54:15 -0300

    add clear_sky module

---
+++ parcelas/clear_sky.py
@@ -0,0 +1,42 @@
+from functools import cached_property
+
+import odc.stac
+import planetary_computer
+import pystac_client
+
+
+def get_landsat_data(shp, time_range="2020-01-01/2020-12-31", bands=["qa_pixel"]):
+    catalog = pystac_client.Client.open(
+        "https://planetarycomputer.microsoft.com/api/stac/v1",
+        modifier=planetary_computer.sign_inplace,
+    )
+
+    search = catalog.search(
+        collections=["landsat-c2-l2"],
+        intersects=shp.unary_union,
+        datetime=time_range,
+    )
+
+    items = search.item_collection()
+
+    # Get rid of L7 data
+    items = [i for i in items if "7" not in i.properties["platform"]]
+    print(f"Found {len(items)} Items")
+
+    return odc.stac.stac_load(
+        items,
+        bands=bands,
+        intersects=shp.unary_union,
+        chunks={"y": 512, "x": 512},
+        nodata=65535,
+    )
+
+
+def compute_clear_sky_percentage(
+    data_ls, clear_sky_qa_flags=[21824, 21826, 21888, 21890, 30048, 54596]
+):
+    qa = data_ls["qa_pixel"]
+    clear_sky = qa.isin(clear_sky_qa_flags)
+    _sum = clear_sky.astype(int).sum(dim="time")
+
+    return _sum / len(qa.time)
