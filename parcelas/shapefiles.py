1e6e9e7a695e21ec9df6e53c2413b00d2da0411a
Giacomo Caria <giacomo@chloris.earth>
2026-01-22 19:38:48 -0300

    add simpler method to load wrs2 grid

--- parcelas/shapefiles.py
+++ parcelas/shapefiles.py
@@ -5,17 +5,24 @@ import zipfile
 import geopandas as gpd
 import requests
 
+URL_WRS2_GRID = "https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/atoms/files/WRS2_descending_0.zip"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
 
-def get_wrs2_grid(output_file="wrs2_descending.geojson"):
-    # Updated direct download URL for WRS-2 Descending Shapefile
-    url = "https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/atoms/files/WRS2_descending_0.zip"
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
+def get_wrs2_grid():
+    return gpd.read_file(URL_WRS2_GRID)
+
+
+def download_wrs2_grid(output_file="wrs2_descending.geojson"):
+
+    if os.path.exists(output_file):
+        print(f"WRS-2 grid already exists at {output_file}.")
+        return
 
     print(f"Downloading WRS-2 from USGS...")
-    response = requests.get(url, headers=headers, stream=True)
+    response = requests.get(URL_WRS2_GRID, headers=HEADERS, stream=True)
 
     if response.status_code != 200:
         print(f"Failed to download. Status code: {response.status_code}")
@@ -40,7 +47,7 @@ def get_wrs2_grid(output_file="wrs2_descending.geojson"):
         gdf = gdf.to_crs(epsg=4326)
         gdf.to_file(output_file, driver="GeoJSON")
         print(f"Success! Saved to {output_file}")
-        return gdf
+        return
     else:
         raise ValueError("Could not find a .shp file in the zip.")
 
